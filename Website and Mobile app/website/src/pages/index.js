d0dfbf3f6c3dd4172aa32ae69349f9d5df2f3f29
Ahda Akmalul Ilmi <ahda043+GitHub@gmail.com>
2023-06-04 20:08:23 +0700

    Adding daily report feature

--- Website and Mobile app/website/src/pages/index.js
+++ Website and Mobile app/website/src/pages/index.js
@@ -17,10 +17,35 @@ export default function Dashboard() {
 
       <div className="text-center m-10">
         <h1 className="font-serif">Status</h1>
-        <div className="min-h-fit m-10 grid grid-cols-3 gap-3">
-          <Progress name="pH Status" percent="50" />
-          <Progress name="Turbidity Status" percent="50" />
-          <Progress name="Temperature Status" percent="50" />
+        <div className="m-10 grid grid-rows-3 gap-3">
+          <div className="grid grid-cols-2 gap-2 items-center justify-center">
+            <Progress name="pH" percent="15" />
+            <div className="text-center">
+              <h2>Daily Report</h2>
+              <table className="daily-report">
+                <tr>
+                  <th>#</th>
+                  <th>Date</th>
+                  <th>Report</th>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>3 Mei 2023</td>
+                  <td>no data</td>
+                </tr>
+              </table>
+            </div>
+          </div>
+          {/* 
+          <div className="grid grid-cols-2 gap-2">
+            <Progress name="Turbidity" percent="0" />
+            <h2>Daily Report</h2>
+          </div>
+
+          <div className="grid grid-cols-2 gap-2">
+            <Progress name="Temperature" percent="0" />
+            <h2>Daily Report</h2>
+          </div> */}
         </div>
       </div>
     </>
