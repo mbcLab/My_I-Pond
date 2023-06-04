eea88f5f0d7f112a69c2d4bb9db359765c9d1e3e
Ahda Akmalul Ilmi <ahda043+GitHub@gmail.com>
2023-06-04 20:05:35 +0700

    Minor update

--- Website and Mobile app/website/src/components/Navbar.js
+++ Website and Mobile app/website/src/components/Navbar.js
@@ -1,14 +1,17 @@
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import Image from "next/image";
 
 export default function Navigation() {
   return (
-    <div className="grid grid-cols-4 max-h-12 m-2 sticky text-center">
+    <div className="grid grid-cols-4 max-h-fit m-2 justify-center items-center text-center">
       <div className="flex justify-center items-center">
         <a href="/">
-          <img src="./logo.png" alt="MBC Laboratory" className="max-h-12" />
+          <Image
+            src="/logo.png"
+            alt="MBC Laboratory"
+            width={250}
+            height={250}
+            sizes="(max-width: 250px)"
+          />
         </a>
       </div>
       <div className="col-span-2 font-mono min-h-fit">
@@ -16,6 +19,7 @@ export default function Navigation() {
       </div>
       <div className="flex justify-center items-center">
         <a href="https://www.instagram.com/mbclab/" target="_blank">
+          {/* <Image src="/logo_circle.ico" alt="button" width={100} height={100} /> */}
           <img
             src="./logo_circle.ico"
             alt="burger"
