6c1e85724b304521250afda4792bc908a12bb8f3
Ahda Akmalul Ilmi <ahda043+GitHub@gmail.com>
2023-06-25 17:19:32 +0700

    Enabling fetching images from unsplash

--- Website and Mobile app/website/next.config.js
+++ Website and Mobile app/website/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ["unsplash.com", "images.unsplash.com"],
+  },
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
